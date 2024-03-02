f89800a339249eb6592611d045fdb71d69123c7c
C64HexCoder <yossi.shelly@gmail.com>
2024-03-02 20:27:44 +0200

    Timer is working

--- OLEDTimer/main.c
+++ OLEDTimer/main.c
@@ -13,27 +13,54 @@
 #include "SevenSeg19H.h"
 //#include "OLED.h"
 
+unsigned char Minuts,Seconds;
+unsigned char Buffer[55][3];
+
+
 int main(void)
 {
 	DDRD = 0x01;
 	//OLED oled;
-		
+	Minuts = 2;
+	Seconds = 0;
+	
 	OLED_Init();  //initialize the OLED
 	OLED_Clear(); //clear the display (for good measure)
 		
-	OLED_SetCursor(0, 0);        //set the cursor position to (0, 0)
-	OLED_DrawBitmapRLE (0,0,CathClockMainScreenRLE,566); //Print out some text
-	OLED_WriteTwoDigitNumber(SevenSegments_struc,37,4,02,false,false,false,false,false);
-	OLED_WriteChar(SevenSegments_struc,59,4,':',false,false,false);
-	OLED_WriteTwoDigitNumber(SevenSegments_struc,70,4,0,false,false,false,false,false);
+			OLED_SetCursor(0, 0);        //set the cursor position to (0, 0)
+			OLED_DrawBitmapRLE (0,0,CathClockMainScreenRLE,566); //Print out some text
+			OLED_WriteTwoDigitNumber(SevenSegments_struc,37,4,02,false,false,false,false,false);
+			OLED_WriteChar(SevenSegments_struc,59,4,':',false,false,false);
+			OLED_WriteTwoDigitNumber(SevenSegments_struc,70,4,0,false,false,false,false,false);
 	
 	
 	while (1)
 	{
 		PORTD ^= 0x01;
-		_delay_ms (100);
+		_delay_ms (1000);
 			
+		if (--Seconds == 255)
+		{
+			if (--Minuts == 255)
+				TimerFinished();
+			else
+				Seconds = 59;
+		}
+	
+	
+			
+		//OLED_SetCursor(0, 0);        //set the cursor position to (0, 0)
+		//OLED_DrawBitmapRLE (0,0,CathClockMainScreenRLE,566); //Print out some text
+		OLED_WriteTwoDigitNumber(SevenSegments_struc,37,4,Minuts,false,false,false,false,false);
+		OLED_WriteChar(SevenSegments_struc,59,4,':',false,false,false);
+		OLED_WriteTwoDigitNumber(SevenSegments_struc,70,4,Seconds,false,false,false,false,false);
 	}
     //TODO:: Please write your application code 
 
+}
+
+void TimerFinished ()
+{
+	Minuts = 2;
+	Seconds = 0;		
 }
